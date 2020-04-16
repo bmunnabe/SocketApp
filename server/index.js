e32d795b1cf014cb104b766f6ebddf2e55a0af14
bmunnabe <bmunnabe@gmail.com>
2020-04-16 22:17:17 +0530

    server added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const socket = require('socket.io');
-const http = require('http');
-const router = require('./router');
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const router = require("./router");
+const { addUser, removeUser, getUsersInRoom, getUser } = require("./user");
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,18 +10,55 @@ const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
-io.on('connection', (socket) => {
-	console.log('Connected with Socket IO Server');
+io.on("connection", (socket) => {
+  console.log("Connected with Socket IO Server");
 
-	socket.on('join', ({ name, room }, callback) => {
-		console.log('Connected on join');
-		console.log(name, room);
-		callback(name, room, 'FuckTest');
-	});
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
 
-	socket.on('disconnect', () => {
-		console.log('disconnected');
-	});
+    //Join room is exist or creating room and join
+    socket.join(user.room);
+
+    // This will be emitted to specific person or client
+    socket.emit("message", {
+      user: "admin",
+      text: `Hi ${user.name}, Welcome to room ${user.room}`,
+    });
+    //this will be emitted to all ROOM people
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name} has joined room`,
+    });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback("Successfully received data" + name);
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} left room ${user.room}`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+    console.log("disconnected");
+  });
 });
 
 app.use(router);
