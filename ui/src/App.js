93915879dc992d5414c328f525db406a86c24dac
bmunnabe <bmunnabe@gmail.com>
2020-04-16 22:17:31 +0530

    lint format

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Join from './components/Join/Join';
-import Chat from './components/Chat/Chat';
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
 
 export class App extends Component {
-	render() {
-		return (
-			<Router>
-				<Route path="/chat" component={Chat}>
-					{/* When we use below type, it doesn't give location, history , match on RFC */}
-					{/* <Chat /> */}
-				</Route>
-				<Route exact path="/">
-					<Join />
-				</Route>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <Route path="/chat" component={Chat}>
+          {/* When we use below type, it doesn't give location, history , match on RFC */}
+          {/* <Chat /> */}
+        </Route>
+        <Route exact path="/">
+          <Join />
+        </Route>
+      </Router>
+    );
+  }
 }
 
 export default App;
